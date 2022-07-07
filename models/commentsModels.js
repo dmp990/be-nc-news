9457dca5b7813ed345ebb7bce6a333f42cf56f06
asad <asadali.asif.1@gmail.com>
2022-07-07 14:17:26 +0100

    implement PATCH /api/comments/:comment_id

--- models/commentsModels.js
+++ models/commentsModels.js
@@ -16,3 +16,28 @@ exports.removeCommentById = async ({ comment_id }) => {
     [+comment_id]
   );
 };
+
+exports.updateCommentById = async ({ comment_id }, { inc_votes }) => {
+  if (isNaN(+comment_id)) {
+    return Promise.reject({ status: 400, msg: "comment_id must be a number" });
+  }
+
+  await checkExists("comments", "comment_id", +comment_id).catch(() => {
+    return Promise.reject({ status: 404, msg: "no comment with this id" });
+  });
+
+  if (isNaN(+inc_votes)) {
+    return inc_votes === undefined
+      ? Promise.reject({ status: 422, msg: "please provide inc_votes" })
+      : Promise.reject({ status: 422, msg: "inc_votes must be a number" });
+  }
+
+  return db
+    .query(
+      `UPDATE comments SET votes = votes + $1 WHERE comment_id = $2 RETURNING *;`,
+      [+inc_votes, +comment_id]
+    )
+    .then(({ rows }) => {
+      return rows[0];
+    });
+};
